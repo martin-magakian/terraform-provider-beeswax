919a72af905b8047b2b64ec34a108fdd46f7452a
Martin Magakian <martin.magakian@gmail.com>
2024-03-28 09:39:22 +0100

    add doc

--- examples/data-sources/beeswax_role/data-source.tf
+++ examples/data-sources/beeswax_role/data-source.tf
@@ -1,4 +1,5 @@
 data "beeswax_role" "example" {
-  # Limitation: it's only possible to retrieve the role by ID for now. TODO: allow finding role by name
+  # Limitation: it's only possible to retrieve the role by ID for now
+  # TODO: allow finding role by name
   id = 42
 }
--- examples/data-sources/beeswax_user/data-source.tf
+++ examples/data-sources/beeswax_user/data-source.tf
@@ -1,4 +1,5 @@
 data "beeswax_user" "example" {
-  # Limitation: it's only possible to retrieve the user by ID for now. TODO: allow finding user by email
+  # Limitation: it's only possible to retrieve the user by ID for now.
+  # TODO: allow finding user by email
   id = 42
 }
